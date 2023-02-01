41f57cca9b7e5f8184f6155b5d23225aa34a4874
chela98 <emre.dlbdk@icloud.com>
2023-02-01 19:20:22 +0300

    first commit

---
+++ src/test/java/com/example/demo/ButRestApiTest.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.*;
+
+import org.junit.jupiter.api.Test;
+
+class ButRestApiTest {
+
+	@Test
+	void testListele() {
+		List<String> sonuc = ButRestApi.listele();
+		
+		assertEquals(ButRestApi.DERSLER, sonuc);
+	}
+
+	@Test
+	void testEkle() {
+		String dersAd = "emre";
+		
+		Boolean sonuc = ButRestApi.ekle(dersAd);
+		
+		assertEquals(true, sonuc);
+	}
+
+	@Test
+	void testSil() {
+		Integer silinecekId = 1;
+		ButRestApi.DERSLER.add("veri");
+		
+		String sonuc = ButRestApi.sil(silinecekId);
+		
+		assertEquals("sildiniz",sonuc);
+		
+	}
+
+}
